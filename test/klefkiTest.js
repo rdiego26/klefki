8b9e0968dc77ac3ec9f052cf3b37533c49e1727f
Diego Ramos <rdiego26@gmail.com>
2015-10-07 18:38:51 -0300

    ADD decrypt tests.

--- test/klefkiTest.js
+++ test/klefkiTest.js
@@ -1,7 +1,7 @@
 /**
  * Created by ramos on 07/10/15.
  */
-var _klefki = require('../index.js');
+var _klefki = require('../lib/klefki.js');
 
 describe('klefki', function() {
 
@@ -31,6 +31,28 @@ describe('klefki', function() {
         it('Valid string and key returns cipher', function(done) {
             var _cipher = _klefki.encrypt('abracadabra', 'myKey');
             if(_cipher.length > 0) done();
+        });
+
+    });
+
+    describe('Decrypt', function() {
+
+        it('Null string returns exception', function(done) {
+            try {
+                _klefki.decrypt(null);
+            } catch (e) {
+                done();
+            }
+        });
+
+        it('Valid cipher string returns decrypt text', function(done) {
+            var _word = 'abracadabra',
+                _cipher = _klefki.encrypt(_word),
+                _plainText;
+
+            _plainText = _klefki.decrypt(_cipher);
+            if(_plainText === _word) done();
+
         });
 
     });
